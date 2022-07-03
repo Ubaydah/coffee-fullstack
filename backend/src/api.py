c5976d5e13a8d19d0fadd9dbe5aeb920a17e9f94
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-07-03 13:45:01 +0100

    chore: implemented the required endpoints 🐍

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,76 +11,79 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
-# db_drop_and_create_all()
+db_drop_and_create_all()
+
+@app.route('/')
+def welcome():
+    return jsonify({
+        "status": "sucesss",
+        "message": "Welcome to coffee shop"
+        })
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    drinks = Drink.query.all()
+    response = [drink.short() for drink in drinks]
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+    return jsonify({
+        "success": True,
+        "drinks": response
+    }), 200
 
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(jwt):
+    print(jwt)
+    drinks = Drink.query.all()
+    response = [drink.long() for drink in drinks]
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+    return jsonify({
+        "success": True,
+        "drinks": response
+    }), 200
 
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drinks(jwt):
+    body = request.get_json()
+    new_title = body['title']
+    new_recipe = json.dumps(body["recipe"])
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+    try:
+        drink = Drink(title=new_title, recipe=new_recipe)
+        drink.insert()
+        return jsonify({
+                "success": True,
+                "drinks": [drink.long()],
+            }), 200
+    
+    except Exception as e:
+        abort(422)
 
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(jwt, id):
+    drink = Drink.query.get(id)
+    id = drink.id
+    if drink is None:
+        abort(404)
+    drink.delete()
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
+    return jsonify({
+        "success": True,
+        "delete": id
+    }), 200
 
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-
+@app.errorhandler(AuthError)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": error.status_code,
+        "message": error.error
+        }), error.status_code
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -90,25 +93,50 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "resource not found"
+        }), 404
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(500)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 500,
+        "message": "Internal Server Error"
+        }), 500
 
-'''
+@app.errorhandler(401)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 401,
+        "message": "Unauthorized Request"
+        }), 401
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
+@app.errorhandler(403)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 403,
+        "message": "Forbidden Request"
+        }), 403
 
+@app.errorhandler(400)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 400,
+        "message": "Bad Request"
+        }), 400
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
+@app.errorhandler(405)
+def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 405,
+        "message": "Method not Allowed"
+        }), 405
